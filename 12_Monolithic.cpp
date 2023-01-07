c7c0b57c791c8e1fd04e10352fcf92528eae2fbb
FirstThinker_Swapnil <92979885+Swap-Nova@users.noreply.github.com>
2023-01-08 02:39:27 +0530

    Basic Programming 👽

---
+++ 12_Monolithic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    int length=0,breadth=0;
+    printf("Enter length & breadth: ");
+    cin>>length>>breadth;
+
+    int area=length*breadth;
+    int peri=2*(length+breadth);
+
+    printf("Area: %d\n Perimeter: %d\n",area,peri);
+
+    return 0;
+}
